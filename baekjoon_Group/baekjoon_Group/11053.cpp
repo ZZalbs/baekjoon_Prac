a91c4b2f41b405cf9635291b0a40d2e7b108b69e
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2022-10-06 14:55:55 +0900

    11053 포문풀이_실패
    1 5 3 4 같이 사이에 비정상적으로 큰 값이 껴있는 경우 구분이 안됨

--- baekjoon_Group/baekjoon_Group/11053.cpp
+++ baekjoon_Group/baekjoon_Group/11053.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-set <long long > s;
 
 int main()
 {
@@ -17,16 +16,29 @@ int main()
 
 	int num;
 	cin >> num;
-	int p;
+	int p[1000] = { 0 };
 	int vs = 0;
+	int cnt=0; // 각 부분수열 개수
+	int res = 0; // 최대 수
+	for (int i = 0; i < num; i++){	
+		cin >> p[i];
+	}
 	for (int i = 0; i < num; i++)
 	{
-		cin >> p;
-		if (vs<p) {
-			s.insert(p);
-			cout << p<<'\n';
-			vs = p;
+		cnt = 1;
+		vs = p[i];
+		//cout << p[i] << "시작\n";
+		for (int j = i; j < num; j++)
+		{
+			if (vs < p[j]) {
+				//cout << p[j] << "<<<<<<<<<<\n";
+				cnt++;
+				vs = p[j];
+			}
 		}
+		if (res < cnt)
+			res = cnt;
 		
 	}
+	cout << res;
 }

05e4da04e773285cf9111469c0846c980c2356c1
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-03-16 14:45:54 +0000

    6549 작업중

--- baekjoon_Group/baekjoon_Group/6549.cpp
+++ baekjoon_Group/baekjoon_Group/6549.cpp
@@ -15,3 +15,34 @@ using namespace std;
 
 const int MAX = 1000001;
 const int INF = 100000001;
+
+int n=-1;
+vector <long long> hist;
+
+long long Square(int left, int right) // 분할정복으로 풀어볼거에요
+{
+    if(left==right) return hist[left];
+    int mid = (left+right)/2;
+    long long res;
+    res=max(Square(left,mid),Square(mid+1,right));
+    
+    while()
+    {
+        
+    }
+}
+
+int main()
+{
+    fastio;
+    while(n!=0)    
+    {
+        hist.clear();
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            cin>>hist[i];
+        }
+        cout<<Square(0,n);
+    }
+}

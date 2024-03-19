fbf93ff877ceab6a38c2e116a865298ab6e2dd88
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-03-19 14:18:03 +0000

    2618 start

---
+++ baekjoon_Group/baekjoon_Group/2618.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cmath>
+#include <string.h>
+#include <cmath>
+#define fastio ios::sync_with_stdio(false);cin.tie(nullptr)
+using namespace std;
+
+const int MAX = 1000001;
+const int INF = 100000001;
+
+int main()
+{
+    
+}

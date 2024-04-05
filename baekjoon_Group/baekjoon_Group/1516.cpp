1b47667e8cb361749e019e4ec5afb634d368e7b9
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-04-05 14:03:46 +0000

    1516 그래프생성 완료

--- baekjoon_Group/baekjoon_Group/1516.cpp
+++ baekjoon_Group/baekjoon_Group/1516.cpp
@@ -13,23 +13,56 @@
 #define fastio ios::sync_with_stdio(false);cin.tie(nullptr)
 using namespace std;
 
-const int MAX = 101;
+const int MAX = 501;
 const int INF = 987654321;
 
 vector<vector<int>> graph; // 그래프
 vector<bool> visited;
+int dp[MAX];
+int n;
+
 
 void DFS(int here)
 {
     visited[here]=true;
-    for(int i=0;i<graph[here].size;i++)
+    for(int i=0;i<graph.size();i++)
     {
         if(!visited[i]) DFS(graph[here][i]);
     }
 }
 
+void PrintGraph()
+{
+    cout<<"\n";
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<graph[i][j];
+        }
+        cout<<"\n";
+    }
+
+}
 
 int main()
 {
-
+    fastio;
+    cin>>n;
+    graph = vector<vector<int>>(n+1,vector<int>(n+1,0));
+    memset(dp,-1,sizeof(dp));
+    int value,need;
+    for(int i=1;i<=n;i++)
+    {
+        value=0;
+        need=0;
+        cin>>value;
+        while(need!=-1)
+        {
+            cin>>need;
+            if(need==-1) break;
+            graph[need][i] = value; 
+        }
+    }
+    PrintGraph();
 }

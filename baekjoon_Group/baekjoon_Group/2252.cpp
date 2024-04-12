536c62fa55bd8c0e7af38476909b931ffbfce91a
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-04-12 11:56:20 +0000

    2252 위상정렬작업중

--- baekjoon_Group/baekjoon_Group/2252.cpp
+++ baekjoon_Group/baekjoon_Group/2252.cpp
@@ -13,11 +13,12 @@
 #define fastio ios::sync_with_stdio(false);cin.tie(nullptr)
 using namespace std;
 
-
+const int MAX = 32001;
 
 queue<int> q; // kahn 위상정렬용 큐
 int n,m;
 vector<vector<int>> graph;
+int nodeCount[MAX]; //간선 개수
 
 void PrintGraph()
 {
@@ -31,15 +32,33 @@ void PrintGraph()
     }
 }
 
+void TopologicalSort()
+{
+    for(int i=1;i<=n;i++)
+    {
+        if(nodeCount[i]==0) q.push(i);
+    }
+    int curNode = q.front();
+    
+    while(!q.empty())
+    {
+        q.pop();
+        cout<<curNode<<" ";
+
+    }
+}
+
 int main()
 {
     fastio;
     int a,b;
+    memset(nodeCount,0,sizeof(nodeCount));
     cin>>n>>m;
     graph.assign(n+1, vector<int>(0,0));
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) // a가 b의 앞이다, 즉 a->b
     {
         cin>>a>>b;
         graph[a].push_back(b);
+        nodeCount[a]++;
     }
 }

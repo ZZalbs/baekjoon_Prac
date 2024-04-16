69dd75dc16f2ad595401f78d4ce7c34bd8adfa3a
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-04-16 14:10:04 +0000

    1766 정답

--- baekjoon_Group/baekjoon_Group/1766.cpp
+++ baekjoon_Group/baekjoon_Group/1766.cpp
@@ -14,3 +14,51 @@
 using namespace std;
 
 const int arrayLimit = 100002;
+
+int questionCount,informationCount;
+vector<vector<int>> informationAboutQuestions;
+int headNodeCount[arrayLimit];
+set<int> setForBFS; // 원래는 우선순위 큐 만들어야되는데, set으로 대체되니까 그냥 set씀
+
+void InputQuestionCntAndInfoCnt(){
+    cin>>questionCount>>informationCount;
+}
+
+void TopologicalSort()
+{
+    for(int i=1;i<=questionCount;i++)
+    {
+        if(headNodeCount[i]==0) setForBFS.insert(i);
+    }
+
+    while(!setForBFS.empty())
+    {
+        int nowNode = *(setForBFS.begin());
+        setForBFS.erase(setForBFS.begin());
+        cout<<nowNode<<" ";
+        for(int i=0;i<informationAboutQuestions[nowNode].size();i++)
+        {
+            int nextNode = informationAboutQuestions[nowNode][i];
+            headNodeCount[nextNode]--;
+            if(headNodeCount[nextNode]==0) setForBFS.insert(nextNode);
+        }
+    }
+}
+
+
+int main()
+{
+    fastio;
+    InputQuestionCntAndInfoCnt();    
+    memset(headNodeCount,0,sizeof(headNodeCount));
+    informationAboutQuestions.assign(arrayLimit,vector<int>(0,0));
+    int firstSolveQues, laterSolveQues;
+    for(int i=0;i<informationCount;i++)
+    {
+        cin>>firstSolveQues>>laterSolveQues;
+        informationAboutQuestions[firstSolveQues].push_back(laterSolveQues);
+        headNodeCount[laterSolveQues]++;
+    }
+
+    TopologicalSort();
+}

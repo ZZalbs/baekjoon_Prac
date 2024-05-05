bb4215092cd34e84b93e96aeab62f3c4c83680ef
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2024-05-05 14:54:58 +0000

    4013 타잔 구현완료_scc 안만들어짐

--- baekjoon_Group/baekjoon_Group/4013.cpp
+++ baekjoon_Group/baekjoon_Group/4013.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 // ic와 레스토랑 모두 정점의 종류임. 목적지는 레스토랑, ic는 목적지가 될 수 없음
 
-const int arrayLimit = 10002;
+const int ARRAYLIMIT = 10002;
 
 
 int icCount,roadCount;
@@ -23,20 +23,64 @@ int icHead,icTail;
 int moneyAmount;
 int startIcNum, restaurantCount;
 vector<vector<int>> road;
-int headNodeCount[arrayLimit];
-int icValue[arrayLimit];
+int headNodeCount[ARRAYLIMIT];
+int icValue[ARRAYLIMIT];
 vector<int> restaurant;
 
+stack<int> stackForScc;
+vector<vector<int>> scc;
+vector<int> sccRow;
+int nodeGroupIdSet[ARRAYLIMIT];
+int nowGroupId;
+bool isDfsFinished[ARRAYLIMIT];
 
 
+int FindSccWithTarzan(int nowNode)
+{
+    nodeGroupIdSet[nowNode]=++nowGroupId;
+    int parentNode = nodeGroupIdSet[nowNode];
+    stackForScc.push(nowNode);
+    
+    for(int i=0;i<road[nowNode].size();i++)
+    {
+        int nextNode = road[nowNode][i];
+        if(nodeGroupIdSet[nextNode] == 0)
+            parentNode = min(parentNode,FindSccWithTarzan(nextNode));
+        else if(!isDfsFinished[nextNode])
+            parentNode = min(parentNode,nodeGroupIdSet[nextNode]);
+    }
 
-int main()
+    if(parentNode == nodeGroupIdSet[nowNode])
+    {
+        while(true)
+        {
+            int targetNode = stackForScc.top();
+            isDfsFinished[targetNode] = true;
+            sccRow.push_back(targetNode);
+            stackForScc.pop();
+            if(targetNode == nowNode) break;
+        }
+        sort(sccRow.begin(),sccRow.end());
+        scc.push_back(sccRow);
+    }
+
+    return parentNode;
+}
+
+void Initialize()
 {
     fastio;
     memset(headNodeCount,0,sizeof(headNodeCount));
     memset(icValue,0,sizeof(icValue));
+    road.assign(ARRAYLIMIT,vector<int>(0,0));
+    memset(nodeGroupIdSet,0,sizeof(nodeGroupIdSet));
+    memset(isDfsFinished,false,sizeof(isDfsFinished));
+    nowGroupId=0;
+}
+
+void GetInput()
+{
     cin>>icCount>>roadCount;
-    road.assign(arrayLimit,vector<int>(0,0));
     for(int i=0;i<roadCount;i++)
     {
         cin>>icHead>>icTail;
@@ -52,4 +96,30 @@ int main()
         cin>>imsiRestaurant;
         restaurant.push_back(imsiRestaurant);
     }
+}
+
+void PrintScc()
+{
+    cout<<"\n";
+    for(int i=0;i<scc.size();i++)
+    {
+        for(int j=0;j<scc[i].size();j++)
+            cout<<scc[i][j]<<" ";
+        cout<<"\n";
+    }
+}
+
+int main()
+{
+    Initialize();
+    GetInput();
+    
+    for(int i=1;i<=icCount;i++)
+    {
+        if(nodeGroupIdSet[i] != 0 )
+            FindSccWithTarzan(i);
+    }
+
+    PrintScc();
+    
 }
